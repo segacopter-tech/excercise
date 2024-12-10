79391985d47e531fc408e24a1fb8eff485110095
T_cat9000_2 <terpstragraham@gmail.com>
2024-12-09 19:13:46 -0800

    docs.py

---
+++ src/docs.py
@@ -0,0 +1,14 @@
+# regular docs generator for segcopter tech project docs
+
+generated_docs = []
+
+print("we are gonna generate some docs starters in maerkdown.")
+generated_docs.append("# docs")
+generated_docs.appened("## about")
+generated_docs.append("text info about your project")
+generated_docs.append("## copyright")
+generated_docs.append("copyright info like the license")
+generated_docs.append("## dependencies")
+generated_docs.append("dependencies for your project")
+generated_docs.append("## install")
+generated_docs.append("how to install your project")
